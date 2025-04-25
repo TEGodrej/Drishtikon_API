84c1305b05f00dde138dc48e92bdd5ba95c30747
testing.engineer <testing.engineer@GAVLMUMDT-046.godrejinds.com>
2025-04-25 09:08:07 +0530

    Added jsonfile

--- src/test/java/ProofOfDelivery/SendingPostRequestToSubmitProofOfDelivery.java
+++ src/test/java/ProofOfDelivery/SendingPostRequestToSubmitProofOfDelivery.java
@@ -1,6 +1,18 @@
 package ProofOfDelivery;
 
+import static io.restassured.RestAssured.given;
+
+import java.io.File;
+import java.io.IOException;
+
 import org.testng.annotations.Test;
+
+import GenericUtility.FileUtility;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class SendingPostRequestToSubmitProofOfDelivery {
 
@@ -8,9 +20,39 @@ public class SendingPostRequestToSubmitProofOfDelivery {
 	String endPoint="ZMOB_PCRM_DISTRIBUTOR_SRV/ProofOfDlvSet";
 	String bearerToken;
 	String csrfToken;
-	
+//	Map<String, String> cookies=CapturedCookies.cookies;
 	@Test
-	public void test() {
-		System.out.println("test");
+	public void test() throws IOException {
+		File jsonFile = new File("C:\\Users\\testing.engineer\\eclipse-workspace\\DirshtikonSAP_RestAPI\\src\\main\\resources\\TestData.json");
+		
+		
+		csrfToken=FileUtility.getDataFromPropertyFile("X_CSRF_TOKEN");
+		bearerToken=FileUtility.getDataFromPropertyFile("bearer_TOKEN");
+		
+		
+		Headers header=new Headers(new Header("Cache-Control","no-Cache"),
+//				new Header("Accept","*/*"),
+				new Header("Accept-Encoding","gzip,deflate,br"),
+				new Header("Connection","keep-alive"),
+				new Header("Accept","application/json"));
+		
+//		token=FileUtility.getDataFromPropertyFile("bearer_TOKEN");
+		
+		
+		Response response = given()
+			    .headers(header)
+			    .header("X-CSRF-Token", csrfToken)
+//			    .cookies(cookies)
+			    .contentType(ContentType.JSON)
+			    .body(jsonFile)
+			    .auth().oauth2(bearerToken)
+			    .baseUri(baseUrl)
+			    .post(endPoint)
+			    .then()
+				.log().all()
+				.extract().response();
+		
+		ValidatableResponse responseBody = response.then().log().body();
+		System.out.println(responseBody);
 	}
 }
